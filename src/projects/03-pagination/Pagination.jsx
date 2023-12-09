20f63b5774f7f559fc7571a0a94699e29fa565c3
pradeep-jais <pradeepjais13@gmail.com>
2023-12-09 15:54:31 +0530

    Error shown for failed products fetch, pagination project completed

--- src/projects/03-pagination/Pagination.jsx
+++ src/projects/03-pagination/Pagination.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Products from './Products';
+import Error from '../../components/Error';
 import './styles.css';
 
 // Dummy product api
@@ -8,17 +9,21 @@ const url = 'https://dummyjson.com/products?limit=100';
 const Pagination = () => {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
+  const [error, setError] = useState({ value: false, errorMessage: null });
 
   const itemsPerPage = 15;
   const totalPages = Math.ceil(products.length / itemsPerPage);
 
+  console.log(error);
   useEffect(() => {
     const fetchData = async () => {
+      setError({ value: false, errorMessage: null });
       try {
         const res = await fetch(url);
         const data = await res.json();
         setProducts(data.products);
       } catch (error) {
+        setError({ value: true, errorMessage: error.message });
         console.log(error);
       }
     };
@@ -31,48 +36,58 @@ const Pagination = () => {
   };
 
   return (
-    <section className="section pagination-project">
-      <div className="section-center products-center">
-        <div className="title">
-          <h3>Products</h3>
-          <div className="underline"></div>
-        </div>
-        <Products products={products} page={page} itemsPerPage={itemsPerPage} />
-        {products.length > 0 && (
-          <div className="pagination">
-            <button
-              onClick={() => {
-                handlePageUpdate(page - 1);
-              }}
-              className={page === 1 ? 'hide-btn' : ''}
-            >
-              ◀
-            </button>
-            {Array.from({ length: totalPages }, (_, i) => {
-              return (
+    <>
+      {error.value ? (
+        <Error errorMessage={error.errorMessage} />
+      ) : (
+        <section className="section pagination-project">
+          <div className="section-center products-center">
+            <div className="title">
+              <h3>Products</h3>
+              <div className="underline"></div>
+            </div>
+            <Products
+              products={products}
+              page={page}
+              itemsPerPage={itemsPerPage}
+            />
+            {products.length > 0 && (
+              <div className="pagination">
                 <button
-                  key={i}
-                  className={page === i + 1 ? 'active-page' : ''}
                   onClick={() => {
-                    handlePageUpdate(i + 1);
+                    handlePageUpdate(page - 1);
                   }}
+                  className={page === 1 ? 'hide-btn' : ''}
                 >
-                  {i + 1}
+                  ◀
                 </button>
-              );
-            })}
-            <button
-              onClick={() => {
-                handlePageUpdate(page + 1);
-              }}
-              className={page === totalPages ? 'hide-btn' : ''}
-            >
-              ▶
-            </button>
+                {Array.from({ length: totalPages }, (_, i) => {
+                  return (
+                    <button
+                      key={i}
+                      className={page === i + 1 ? 'active-page' : ''}
+                      onClick={() => {
+                        handlePageUpdate(i + 1);
+                      }}
+                    >
+                      {i + 1}
+                    </button>
+                  );
+                })}
+                <button
+                  onClick={() => {
+                    handlePageUpdate(page + 1);
+                  }}
+                  className={page === totalPages ? 'hide-btn' : ''}
+                >
+                  ▶
+                </button>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </section>
+        </section>
+      )}
+    </>
   );
 };
 export default Pagination;
