7d80402c296829322141be8c1d55a9785edccf40
pradeep-jais <pradeepjais13@gmail.com>
2023-12-09 16:09:47 +0530

    progress bar project initial setup

---
+++ src/projects/04-progressBar/ProgressBar.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+const ProgressBar = () => {
+  return (
+    <section className="section progress-bar">
+      <div className="section-center">
+        <h3>ProgressBar</h3>
+      </div>
+    </section>
+  );
+};
+export default ProgressBar;
