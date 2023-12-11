9f31ad9bf1c0e339e932994a344ed78f37248f6f
pradeep-jais <pradeepjais13@gmail.com>
2023-12-11 21:59:12 +0530

    custom hook to generate password implemented, error case handled

--- src/projects/06-password-generator/components/PasswordGenerator.jsx
+++ src/projects/06-password-generator/components/PasswordGenerator.jsx
@@ -1,22 +1,24 @@
 import { useState } from 'react';
 import { characterData } from '../data/characterData';
 import '../styles.css';
+import usePasswordGenerator from '../hooks/usePasswordGenerator';
 
 const PasswordGenerator = () => {
   const [passwordLength, setPasswordLength] = useState(4);
-
-  const [checkboxState, setCheckboxState] = useState(characterData);
+  const [checkboxData, setCheckboxData] = useState(characterData);
+  // custom hook
+  const { error, password, generatePassword } = usePasswordGenerator();
+  // console.log(error, password);
 
   const handleLength = (e) => {
     const length = e.target.value;
     setPasswordLength(length);
   };
 
-  const handleCheckboxState = (index) => {
+  const handleCheckboxData = (index) => {
     const checkboxData = [...characterData];
-    checkboxData[index].state = !characterData[index].state;
-    console.log(checkboxData);
-    setCheckboxState(checkboxData);
+    checkboxData[index].state = !checkboxData[index].state;
+    setCheckboxData(checkboxData);
   };
 
   return (
@@ -30,10 +32,13 @@ const PasswordGenerator = () => {
 
         <div className="container">
           {/* password */}
-          <header className="password">
-            <p className="pwd">d23wf f-@#$ddd</p>
-            <button className="btn copyBtn">copy</button>
-          </header>
+          {error || (
+            <header className="password">
+              <p className="pwd">{password}</p>
+              <button className="btn copyBtn">copy</button>
+            </header>
+          )}
+
           {/* password length */}
           <div className="length">
             <span>
@@ -50,7 +55,7 @@ const PasswordGenerator = () => {
           </div>
           {/* checkboxes */}
           <div className="checkboxes">
-            {checkboxState.map((checkbox, index) => {
+            {checkboxData.map((checkbox, index) => {
               return (
                 <div key={index} className="checkbox">
                   <label>
@@ -58,7 +63,7 @@ const PasswordGenerator = () => {
                       type="checkbox"
                       checked={checkbox.state}
                       onChange={() => {
-                        handleCheckboxState(index);
+                        handleCheckboxData(index);
                       }}
                     />
                     {checkbox.title}
@@ -67,15 +72,25 @@ const PasswordGenerator = () => {
               );
             })}
           </div>
-          {/* strength */}
-          <div className="strength">
-            <span>strength:</span>
-            <span>good</span>
-          </div>
-          {/* error */}
-          <p className="error-msg">Select at least one checkbox</p>
+          {/* strength or error*/}
+          {error ? (
+            <p className="error-msg">Select at least one checkbox</p>
+          ) : (
+            <div className="strength">
+              <span>strength:</span>
+              <span>good</span>
+            </div>
+          )}
+
           {/* generate button */}
-          <button className="btn generateBtn">generate password</button>
+          <button
+            className="btn generateBtn"
+            onClick={() => {
+              generatePassword(passwordLength, checkboxData);
+            }}
+          >
+            generate password
+          </button>
         </div>
       </div>
     </section>
---
+++ src/projects/06-password-generator/hooks/usePasswordGenerator.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+const usePasswordGenerator = () => {
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+
+  const generatePassword = (length, checkboxData) => {
+    let charset = '',
+      password = '';
+
+    checkboxData.forEach((checkbox) => {
+      if (checkbox.state) {
+        switch (checkbox.title) {
+          case 'Include Uppercase Letters':
+            charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            break;
+          case 'Include Lowercase Letters':
+            charset += 'abcdefghijklmnopqrstuvwxyz';
+            break;
+          case 'Include Numbers':
+            charset += '1234567890';
+            break;
+          case 'Include Symbols':
+            charset += '~!#$%^&*_+-';
+            break;
+          default:
+            charset = '';
+            break;
+        }
+      }
+    });
+
+    // password algorithm
+    if (!charset) {
+      setError(true);
+      return;
+    }
+
+    for (let i = 0; i < length; i++) {
+      password += charset[Math.floor(Math.random() * charset.length)];
+    }
+    setPassword(password);
+    setError(false);
+  };
+  return { error, password, generatePassword };
+};
+export default usePasswordGenerator;
