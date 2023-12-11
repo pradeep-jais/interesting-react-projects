9b46fdf06e87bfdb71315110a894fd1c074f11a1
pradeep-jais <pradeepjais13@gmail.com>
2023-12-11 15:02:32 +0530

    password generator app intial folder setup

---
+++ src/projects/06-password-generator/components/PasswordGenerator.jsx
@@ -0,0 +1,14 @@
+const PasswordGenerator = () => {
+  return (
+    <section className="pwg-app">
+      <div className="section-center">
+        <div className="title">
+          <h3>Password generator</h3>
+          <div className="underline"></div>
+        </div>
+        <div className="container">app UI</div>
+      </div>
+    </section>
+  );
+};
+export default PasswordGenerator;
---
+++ src/projects/06-password-generator/data/characterData.js
@@ -0,0 +1,6 @@
+export const characterData = [
+  { title: 'Include Uppercase Letters', state: false },
+  { title: 'Include Lowercase Letters', state: false },
+  { title: 'Include Numbers', state: false },
+  { title: 'Include Symbols', state: false },
+];
