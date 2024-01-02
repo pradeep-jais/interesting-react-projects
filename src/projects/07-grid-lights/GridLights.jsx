95e3c69fbcb1503ec5d1d42b9dcf1b5e2170353e
pradeep-jais <pradeepjais13@gmail.com>
2024-01-02 20:35:13 +0530

    grid light cell activating and deactiving in sequence done

--- src/projects/07-grid-lights/GridLights.jsx
+++ src/projects/07-grid-lights/GridLights.jsx
@@ -1,6 +1,70 @@
+import { useState } from 'react';
 import './styles.css';
 
+const cellStructure = [1, 1, 1, 1, 0, 1, 1, 1, 1];
+
 const GridLights = () => {
-  return <div>GridLights</div>;
+  const [order, setOrder] = useState([]);
+  const [isDeactivating, setIsDeactivating] = useState(false);
+
+  const activateCell = (index) => {
+    let newOrder = [...order];
+    if (!newOrder.includes(index)) {
+      newOrder = [...order, index];
+    } else {
+      newOrder = newOrder.filter((i) => i != index);
+    }
+
+    setOrder(newOrder);
+
+    // deactivating cells
+    if (newOrder.length === cellStructure.filter(Boolean).length) {
+      deactivateCells();
+    }
+  };
+
+  const deactivateCells = () => {
+    setIsDeactivating(true);
+    const id = setInterval(() => {
+      setOrder((currentOrder) => {
+        const newOrder = [...currentOrder];
+        newOrder.pop();
+
+        if (newOrder.length < 1) {
+          clearInterval(id);
+          setIsDeactivating(false);
+        }
+
+        return newOrder;
+      });
+    }, 300);
+  };
+  console.log(order);
+
+  return (
+    <section className="section grid-light">
+      <div className="section-center">
+        <div className="title">
+          <h3>grid lights</h3>
+          <div className="underline"></div>
+        </div>
+        <div className="grid-container">
+          {cellStructure.map((cell, index) => {
+            return cell ? (
+              <button
+                key={index}
+                className={order.includes(index) ? 'cell cell-active' : 'cell'}
+                disabled={isDeactivating}
+                aria-label={`cell ${index + 1}`}
+                onClick={() => activateCell(index)}
+              ></button>
+            ) : (
+              <div key={index}></div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 export default GridLights;
