129c1b5f1e0eca8fcea72150af79e230c5595ef0
pradeep-jais <pradeepjais13@gmail.com>
2024-02-16 17:40:04 +0530

    dummy user data fetched and displayed as search suggestion

--- src/projects/10-multi-select-search/MultiSelectSearch.jsx
+++ src/projects/10-multi-select-search/MultiSelectSearch.jsx
@@ -1,6 +1,75 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
+import axios from 'axios';
+import ErrorPage from '../../components/Error';
+
+const USER_API = 'https://dummyjson.com/users/search?q=';
 
 const MultiSelectSearch = () => {
-  return <div>MultiSelectSearch</div>;
+  const [searchTerm, setSearchTerm] = useState('');
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (searchTerm.length < 1) {
+      setUsers([]);
+      return;
+    }
+    const fetchUser = async () => {
+      setIsLoading(true);
+      try {
+        const { data } = await axios.get(`${USER_API}${searchTerm}`);
+        setUsers(data.users);
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchUser();
+  }, [searchTerm]);
+
+  console.log(users);
+
+  if (error) {
+    return <ErrorPage errorMessage={error} />;
+  }
+
+  return (
+    <section className="multi-select-search">
+      <div className="section-center">
+        <div className="search-input">
+          {/* Pills */}
+          <div>
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder="search for a user..."
+            />
+          </div>
+        </div>
+        {isLoading ? (
+          <div className="loading" style={{ marginTop: '3rem' }}></div>
+        ) : (
+          <div className="search-suggestion">
+            {users.map((user) => {
+              const { firstName, lastName, email, image } = user;
+              return (
+                <div key={email} className="user-name">
+                  <img src={image} alt={firstName} />
+                  <span>
+                    {firstName} {lastName}
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 export default MultiSelectSearch;
