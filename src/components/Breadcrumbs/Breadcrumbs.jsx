d0d30c8d0d257386b924828435918585ed9e96c4
pradeep-jais <pradeepjais13@gmail.com>
2024-02-29 15:12:07 +0530

    Breadcrumb dynamic page path for link tag is fixed

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -3,6 +3,7 @@ import './styles.css';
 
 const Breadcrumbs = () => {
   const { pathname } = useLocation();
+  let newPath = '';
 
   const breadcrumbs = pathname.split('/').filter((item) => item);
 
@@ -13,12 +14,13 @@ const Breadcrumbs = () => {
       ) : (
         <div className="breadcrumbs section-center">
           <Link to="/">Home /</Link>
-          {breadcrumbs.map((item, index) => {
+          {breadcrumbs.map((path, index) => {
+            newPath = newPath + `/${path}`;
             return index === breadcrumbs.length - 1 ? (
-              <span key={index}>{item}</span>
+              <span key={index}>{path}</span>
             ) : (
-              <Link key={index} to={`/${item}`}>
-                {`${item} /`}
+              <Link key={index} to={newPath}>
+                {`${path} /`}
               </Link>
             );
           })}
