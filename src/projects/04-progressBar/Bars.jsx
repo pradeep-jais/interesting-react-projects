c7f1a0921edcb3a19c00bff5e3ce578ca289849f
pradeep-jais <pradeepjais13@gmail.com>
2025-03-13 21:25:02 +0530

    bad setState warning fixed, progress bar component modified to a reusable and customizable

--- src/projects/04-progressBar/Bars.jsx
+++ src/projects/04-progressBar/Bars.jsx
@@ -1,13 +1,35 @@
-const Bars = ({ count = 0, onComplete = () => {} }) => {
-  if (count === 100) {
-    onComplete();
-  }
+import { useEffect, useState } from 'react';
+
+const Bars = ({ value = 50, isDynamic }) => {
+  const [count, setCount] = useState(value);
+
+  useEffect(() => {
+    if (!isDynamic) return;
+
+    let id;
+    if (count === 100) {
+      clearTimeout(id);
+      return;
+    }
+
+    id = setTimeout(() => {
+      setCount(count + 1);
+    }, 100);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, [count]);
 
   return (
-    <div className="bars">
-      <span style={{ color: count > 49 ? '#fff' : '#000' }}>{count}%</span>
-      <div style={{ right: `${100 - count}%` }}></div>
-    </div>
+    <>
+      <div className="bars">
+        <span style={{ color: count > 49 ? '#fff' : '#000' }}>{count}%</span>
+        <div style={{ right: `${100 - count}%` }}></div>
+      </div>
+      {isDynamic &&
+        (count === 100 ? <span>Success!</span> : <span>Loading...</span>)}
+    </>
   );
 };
 export default Bars;
--- src/projects/04-progressBar/ProgressBar.jsx
+++ src/projects/04-progressBar/ProgressBar.jsx
@@ -1,48 +1,15 @@
-import { useEffect, useState } from 'react';
 import './styles.css';
 import Bars from './Bars';
 const ProgressBar = () => {
-  const [count, setCount] = useState(0);
-  const [success, setSuccess] = useState(false);
-
-  const [stopCount, setStopCount] = useState(false);
-
-  useEffect(() => {
-    if (count === 100) {
-      setStopCount(true);
-    }
-  }, [count]);
-
-  useEffect(() => {
-    let id;
-    if (count < 100) {
-      id = setInterval(() => {
-        setCount((val) => {
-          // console.log(val, 'interval');
-          return val + 1;
-        });
-      }, 100);
-    }
-    return () => {
-      // console.log('cleanup, interval cleared');
-      clearInterval(id);
-    };
-  }, [stopCount]);
-
   return (
     <section className="section">
       <div className="section-center progress-bar-center">
         <div className="title">
           <h3>Progress bar</h3>
+          <div className="underline"></div>
         </div>
-        <Bars
-          count={count}
-          onComplete={() => {
-            setSuccess(true);
-          }}
-        />
-        {success ? <span>Success!</span> : <span>Loading...</span>}
-        <Bars count={75} />
+        <Bars value={0} isDynamic={true} />
+        <Bars value={75} isDynamic={false} />
       </div>
     </section>
   );
